4b081db5a8a837da9b44234086f5cd6166d98360
charmoniumq <sag150430@utdallas.edu>
2016-06-20 15:34:11 -0500

    added arduino sketch

---
+++ arduino_main/arduino_main.ino
@@ -0,0 +1,16 @@
+int xPin = 0;
+int yPin = 1;
+
+void setup() {
+  Serial.begin(19200);
+}
+
+void loop() {
+  int xVal = analogRead(xPin);
+  int yVal = analogRead(yPin);
+  Serial.print(xVal);
+  Serial.print(',');
+  Serial.print(yval);
+  Serial.println();
+}
+
